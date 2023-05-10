80e4ab283da3e9a95e203badd17496f4b7d96a67
cami5326 <59394407+githubemail5326@users.noreply.github.com>
2023-05-10 00:20:43 -0400

    Update oil displacement.js
    Update the oil displacement js file with grouped by year, dropping duplicates

--- Static/js/oil displacement.js
+++ Static/js/oil displacement.js
@@ -1,33 +1,43 @@
-// create a function to hold the filter params
-function parameterFilter(row) {
-    //return row.parameter == "EV sales" &&
-    return row.parameter == "Oil displacement, million lge" &&
+// create a function to hold the filter parameters
+function parameterFilter(row)
+{
+    return row.parameter == "Oil displacement Mbd" &&
     row.region == "World" &&
-    row.mode == "Cars" &&
-    row.powertrain == "EV" &&
-    row.category == "Historical"     
+    row.mode =="Cars" &&
+    row.powertrain =="EV" &&
+    row.category =="Historical"    
 };
 // create a function to load the csv file and plot the charts
-function createChart() {
-d3.csv("../../Data/dataset.csv").then(function(data){
+function createChart(){
+    d3.csv("../../Data/dataset.csv").then(function(data){
 // filter results
     let filterResult = data.filter(parameterFilter);
-//console.log(filterResult.map(entry => parseFloat(entry.value)))
-    let trace = {
-        y: filterResult.map(entry => parseFloat(entry.value)),
-        x: filterResult.map(entry => entry.year),
-        type: "line"  
-    };
-    console.log(trace)
+ // group the data by year and get the maximum value for each group
+    let groupedData = d3.nest()
+    .key(function(d) { return d.year; })
+    .rollup(function(v) { return d3.max(v, function(d) { return parseFloat(d.value); }); })
+    .entries(filterResult);
+//create graph
+let trace = {
+ y: groupedData.map(entry => entry.value),
+ x: groupedData.map(entry => entry.key),
+ type: "line"  
+};
 // data trace array
     let traceData = [trace];
 // apply the title
     let layout = {
-        title: "Oil displacement Over the Years"
+        title: "Global Oil displacement Over the Years: EV Cars",
+        xaxis: {
+            title: "Year"
+        },
+        yaxis: {
+            title: "Oil Displacement (Million Barrels per Day)"
+        }
     };
 // render the plot to Plotly
     Plotly.newPlot("plot1", traceData, layout);
 })
 };
-//call the function
+//call the createChart function
 createChart();
